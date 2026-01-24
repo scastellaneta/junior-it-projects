266fdc0473a72b6140cf60921f1f9fe5882043eb
scastellaneta <salvatorecastellaneta2002@gmail.com>
2026-01-24 11:39:49 +0100

    Add files via upload

---
+++ rubrica_breve.py
@@ -0,0 +1,23 @@
+
+#rubrica_breve.py
+"""
+Mini progetto Python - Rubrica contatti
+Mostra gestione lista di contatti con aggiunta e visualizzazione.
+
+"""
+rubrica = []
+
+while True:
+    comando = input("Aggiungi (a), Visualizza(v), Esci(q):").lower()
+    if comando == "a":
+        nome = input("Nome:")
+        telefono = input("Telefono:")
+        rubrica.append((nome, telefono))
+    elif comando == "v":
+        for n, t in rubrica:
+            print(f"{n} - {t}")
+    elif comando == "q":
+        break
+    else:
+        print("Comando non valido.")
+
