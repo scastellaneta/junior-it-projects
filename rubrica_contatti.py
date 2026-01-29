5a7cb80c5b1f3a77ed7f337458809356cb9ced7f
scastellaneta <salvatorecastellaneta2002@gmail.com>
2026-01-29 16:46:47 +0100

    to-do_list.py

--- rubrica_contatti.py
+++ rubrica_contatti.py
@@ -1,25 +1,52 @@
 Python
-#rubrica_contatti.py
-"""
-Mini progetto Python - Rubrica contatti
-Mostra gestione lista di contatti con aggiunta e visualizzazione.
-
-"""
-rubrica = []
-
-while True:
-    comando = input("Aggiungi (a), Visualizza(v), Esci(q):").lower()
-    if comando == "a":
-        nome = input("Nome:")
-        telefono = input("Telefono:")
-        rubrica.append((nome, telefono))
-    elif comando == "v":
-        for n, t in rubrica:
-            print(f"{n} - {t}")
-    elif comando == "q":
-        break
-    else:
-        print("Comando non valido.")
 
 
+import json
 
+FILE = "tasks.json"
+
+def carica_task():
+    try:
+        with open(FILE, "r") as f:
+            return json.load(f)
+    except:
+        return []
+            
+def salva_task(tasks):
+    with open(FILE, "w") as f:
+        json.dump(tasks, f, indent=4)
+        
+def aggiungi_task(tasks):
+    titolo = input("Nuova task: ")
+    tasks.append({"titolo":titolo, "completata":False})
+    salva_task(tasks)
+    
+def mostra_task(tasks):    
+    if not tasks:
+        print("Nessuna task.")
+        return
+        
+    for i, t in enumerate(tasks):
+        stato = "✔" if t["completata"] else "✘"
+        print(f"{i+1}. {t['titolo']} [{stato}]")
+        
+def menu():
+    tasks = carica_task()
+    
+    while True:
+        print("\n1. Aggiungi task")
+        print("2. Mostra task")
+        print("3. Esci")
+        
+        scelta = input("Scelta:")
+        
+        if scelta == "1":
+            aggiungi_task(tasks)
+        elif scelta == "2":
+            mostra_task(tasks)
+        elif scelta == "3":
+            break
+        else:
+            print("Scelta non valida.")
+        
+menu()        
