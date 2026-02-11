bc0a81cdac2c561e568590df9bf8f06cdbc83c26
scastellaneta <salvatorecastellaneta2002@gmail.com>
2026-02-11 21:38:39 +0100

    generatore_password.py

--- generatore_password.py
+++ generatore_password.py
@@ -3,13 +3,17 @@ Python
 import random
 import string 
 
+# scelta unghezza
 lunghezza = int(input("Lunghezza password: "))
 
+# caratteri utilizzabili
 caratteri = string.ascii_letters + string.digits
 
+# creazione password
 password = ""
 
 for i in range(lunghezza):
     password += random.choice(caratteri)
-    
+
+# risultato
 print("Password generata:", password)
